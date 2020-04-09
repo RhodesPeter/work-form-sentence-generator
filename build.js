bbe2221a57bff49c0508ec7a66c78767d66e8b35
Peter Rhodes <rhodespeter@hotmail.co.uk>
2020-04-10 00:00:38 +0100

    Clean up js

--- build.js
+++ build.js
@@ -66,38 +66,18 @@ var myBundle = (function (exports) {
         '#65E7C6'
     ];
 
-    // This can be combined with the 2 functions below
-    const preloadPeopleWords = () => {
-        const list = document.querySelector('.words__list--1');
-        const listItems = people.map(word => `<li class="words__list-item">${word}</li>`);
+    const preloadWords = (listClassName, words) => {
+        const list = document.querySelector(listClassName);
+        const listItems = words.map(word => `<li class="word-generator__list-item">${word}</li>`);
 
         list.innerHTML = '';
         list.insertAdjacentHTML('afterbegin', listItems.join(', '));
     };
 
-    const preloadPlaceWords = () => {
-        const list = document.querySelector('.words__list--2');
-        const listItems = places.map(word => `<li class="words__list-item">${word}</li>`);
-
-        list.innerHTML = '';
-        list.insertAdjacentHTML('afterbegin', listItems.join(', '));
-    };
-
-    const preloadLocationWords = () => {
-        const list = document.querySelector('.words__list--3');
-        const listItems = locations.map(word => `<li class="words__list-item">${word}</li>`);
-
-        list.innerHTML = '';
-        list.insertAdjacentHTML('afterbegin', listItems.join(', '));
-    };
-
-    const preloadColourWords = () => {
-        const list = document.querySelector('.colours__list');
-        const listItems = colours.map(color => `<li class="colours__list-item">${color}</li>`);
-
-        list.innerHTML = '';
-        list.insertAdjacentHTML('afterbegin', listItems.join(', '));
-    };
+    const preloadPeopleWords = () => preloadWords('.words__list--1', people);
+    const preloadPlaceWords = () => preloadWords('.words__list--2', places);
+    const preloadLocationWords = () => preloadWords('.words__list--3', locations);
+    const preloadColourWords = () => preloadWords('.colours__list', colours);
 
     const makeGif = () => {  
         const canvas = document.querySelector('.canvas');
